515e57654105c5958edf54931c909f77e8af4579
João Lucas <joaolkbraga01@gmail.com>
2022-04-07 20:22:51 -0300

    Update calculadora

--- calculadora.py
+++ calculadora.py
@@ -3,20 +3,19 @@ from math import *
 
 def evaluate(event):
     try:
-        entry.insert("end", " = " + str(eval(entry.get())))
+        entry.insert('end', ' = '+str(eval(entry.get())))
     except:
-        entry.delete("0","end")
-        entry.insert("end","Syntax Error")
-    
+        entry.delete('0','end')
+        entry.insert('end','Syntax Error')    
 
 w = tk.Tk()
-w.title = "caluculadora"
-w.geometry("400x70")
+w.title('calculadora')
+w.geometry('300x70')
 
-tk.Label(w, text="DIGITE A EXPRESSÃO MATEMÁTICA").pack()
+tk.Label(w, text='DIGITE A EXPRESSÃO MATEMÁTICA').pack()
 
-entry = tk.Entry(w)
-entry.bind("<Return>", evaluate)
+entry = tk.Entry(w,width=30)
+entry.bind('<Return>', evaluate)
 entry.pack()
 
 w.mainloop()
